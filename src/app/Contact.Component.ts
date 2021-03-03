5c8f048588b950b1835dcb783b19ab5912d5dc91
polinenizz <polinenizz@gmail.com>
2021-03-03 18:33:47 +0530

    Initial commit

---
+++ src/app/Contact.Component.ts
@@ -0,0 +1,13 @@
+import { style } from '@angular/animations';
+import {Component,Input} from '@angular/core'
+
+@Component({
+  selector:'contact',
+  template : '<p> <strong>Phone:</strong> {{phone}} , <strong>Email:</strong>{{Email}}</p>',
+  styles : [`p { font-family: Times New Roman; }`]
+})
+export class ContactComponent{
+  @Input() phone:string;
+  @Input() Email:string;
+
+}
---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component, VERSION } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  name = 'Vaidika Rao Polineni, Welcome to your app';
+  phone:string = '9177021144';
+  Email:string = 'polinenizz@gmail.com';
+
+}
